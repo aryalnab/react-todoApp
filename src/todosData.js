030f8871f840cdc8bfcc31dacf2c118993d13606
Aryal Nabin <s1900519@edu.bc.fi>
2019-08-09 14:40:23 +0300

    First Commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Learn react",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "CS50 Online Course",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Make projects",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Practice  Volleyball",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Read books",
+        completed: false
+    }
+]
+
+export default todosData
